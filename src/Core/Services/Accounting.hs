43fa5e1764b7be45bbd15cb01c6a90558e06bcd8
Domini Montessori <dominicusin@duck.com>
2026-09-07 16:42:04 +0300

    fix: use qualified import for Finance.Accounting in Core.Services/Accounting

--- src/Core/Services/Accounting.hs
+++ src/Core/Services/Accounting.hs
@@ -9,7 +9,7 @@ import Data.Int (Int64)
 import Data.Text (Text)
 import Data.Time (UTCTime, getCurrentTime)
 import Data.Map.Strict (Map)
-import Finance.Accounting
+import qualified Finance.Accounting as FA
 
 -- | Accounting event store (stub)
 data AccountingEventStore = AccountingEventStore
@@ -20,30 +20,18 @@ data AccountingEvent = AccountingEvent
   , accountingEventType :: !Text
   } deriving (Show, Eq)
 
--- | Transaction (stub)
-data Transaction = Transaction
-  { transactionId :: !Int64
-  , transactionEntries :: ![Entry]
-  } deriving (Show, Eq)
-
--- | Entry (stub)
-data Entry = Entry
-  { entryId :: !Int64
-  , entryAmount :: !Double
-  } deriving (Show, Eq)
-
 -- | Validate transaction (stub)
-validateTransaction :: Transaction -> Either Text Transaction
+validateTransaction :: FA.Transaction -> Either Text FA.Transaction
 validateTransaction tx = Right tx
 
 -- | Emit entry event (stub)
-emitEntryEvent :: AccountingEventStore -> UTCTime -> Int64 -> Entry -> IO (Either Text ())
+emitEntryEvent :: AccountingEventStore -> UTCTime -> Int64 -> FA.LedgerEntry -> IO (Either Text ())
 emitEntryEvent _ _ _ _ = return $ Right ()
 
 -- | Process transaction with events (stub)
-processTransactionWithEvents :: AccountingEventStore -> Transaction -> IO (Either Text ())
+processTransactionWithEvents :: AccountingEventStore -> FA.Transaction -> IO (Either Text ())
 processTransactionWithEvents _ _ = return $ Right ()
 
 -- | Process transaction with events returning events (stub)
-processTransactionWithEvents' :: AccountingEventStore -> Transaction -> IO (Either Text [AccountingEvent])
+processTransactionWithEvents' :: AccountingEventStore -> FA.Transaction -> IO (Either Text [AccountingEvent])
 processTransactionWithEvents' _ _ = return $ Right []
