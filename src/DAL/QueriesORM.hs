b7cf5dbb204b6ac58801e291c13c62df6669bf18
Domini Montessori <dominicusin@duck.com>
2026-09-08 14:02:25 +0300

    fix: add unKey import to DAL.QueriesORM

--- src/DAL/QueriesORM.hs
+++ src/DAL/QueriesORM.hs
@@ -25,7 +25,7 @@ module DAL.QueriesORM
 import Data.Int (Int64)
 import Data.Text (Text)
 import qualified Data.Text as T
-import Database.Persist.Sql (toSqlKey, rawSql, Single(..), PersistValue(..), Key, Entity(..))
+import Database.Persist.Sql (toSqlKey, rawSql, Single(..), PersistValue(..), Key, Entity(..), unKey)
 import Database.Persist.Postgresql (ConnectionPool)
 import DAL.Schema
 import DAL.Types
