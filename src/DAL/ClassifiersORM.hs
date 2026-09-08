66a6dccabea38bc034e69b764fa9d242a8c14509
Domini Montessori <dominicusin@duck.com>
2026-09-09 02:15:56 +0300

    fix: simplify ClassifiersORM parseClassifier to take Single PersistValue

--- src/DAL/ClassifiersORM.hs
+++ src/DAL/ClassifiersORM.hs
@@ -1,42 +1,13 @@
 -- | DAL ClassifiersORM - raw SQL queries for classifiers
 module DAL.ClassifiersORM
-  ( -- * OKSM
-    getOksmAll
-  , getOksmById
-  , getOksmByCode
-  , -- * OKV
-    getOkvAll
-  , getOkvById
-  , getOkvByCode
-  , -- * OKEI
-    getOkeiAll
-  , getOkeiById
-  , getOkeiByCode
-  , -- * OKPD2
-    getOkpd2All
-  , getOkpd2ById
-  , getOkpd2ByCode
-  , getOkpd2ByParent
-  , -- * OKVED2
-    getOkved2All
-  , getOkved2ById
-  , getOkved2ByCode
-  , getOkved2ByParent
-  , -- * TNVED
-    getTnvedAll
-  , getTnvedById
-  , getTnvedByCode
-  , getTnvedByParent
-  , -- * OKATO
-    getOkatoAll
-  , getOkatoById
-  , getOkatoByCode
-  , getOkatoByParent
-  , -- * OKTMO
-    getOktmoAll
-  , getOktmoById
-  , getOktmoByCode
-  , getOktmoByParent
+  ( getOksmAll, getOksmById, getOksmByCode
+  , getOkvAll, getOkvById, getOkvByCode
+  , getOkeiAll, getOkeiById, getOkeiByCode
+  , getOkpd2All, getOkpd2ById, getOkpd2ByCode, getOkpd2ByParent
+  , getOkved2All, getOkved2ById, getOkved2ByCode, getOkved2ByParent
+  , getTnvedAll, getTnvedById, getTnvedByCode, getTnvedByParent
+  , getOkatoAll, getOkatoById, getOkatoByCode, getOkatoByParent
+  , getOktmoAll, getOktmoById, getOktmoByCode, getOktmoByParent
   ) where
 
 import Data.Int (Int64)
@@ -46,7 +17,6 @@ import Database.Persist.Sql (rawSql, Single(..), PersistValue(..))
 import Database.Persist.Postgresql (ConnectionPool)
 import DAL.Database (ConnectionPool, runDb)
 
--- | Classifier item
 data ClassifierItem = ClassifierItem
   { classifierItemId :: !Int64
   , classifierItemCode :: !Text
@@ -55,34 +25,29 @@ data ClassifierItem = ClassifierItem
   , classifierItemParentId :: !(Maybe Int64)
   } deriving (Show, Eq)
 
--- | Extract Int64 from PersistValue
 persistToInt64 :: PersistValue -> Int64
 persistToInt64 (PersistInt64 n) = n
 persistToInt64 (PersistDouble n) = round n
 persistToInt64 _ = 0
 
--- | Extract Text from PersistValue
 persistToText :: PersistValue -> Text
 persistToText (PersistText t) = t
 persistToText (PersistInt64 n) = T.pack $ show n
 persistToText (PersistDouble n) = T.pack $ show n
 persistToText _ = T.empty
 
--- | Extract Maybe Text from PersistValue
 persistToMaybeText :: PersistValue -> Maybe Text
 persistToMaybeText PersistNull = Nothing
 persistToMaybeText v = Just $ persistToText v
 
--- | Extract Maybe Int64 from PersistValue
 persistToMaybeInt64 :: PersistValue -> Maybe Int64
 persistToMaybeInt64 PersistNull = Nothing
 persistToMaybeInt64 (PersistInt64 n) = Just n
 persistToMaybeInt64 (PersistDouble n) = Just $ round n
 persistToMaybeInt64 _ = Nothing
 
--- | Parse classifier from a list of PersistValue
-parseClassifier :: [PersistValue] -> ClassifierItem
-parseClassifier (id':code:name:desc:parent:_) =
+parseClassifier :: [Single PersistValue] -> ClassifierItem
+parseClassifier (Single id' : Single code : Single name : Single desc : Single parent : _) =
   ClassifierItem
     { classifierItemId = persistToInt64 id'
     , classifierItemCode = persistToText code
@@ -92,23 +57,18 @@ parseClassifier (id':code:name:desc:parent:_) =
     }
 parseClassifier _ = ClassifierItem 0 T.empty T.empty Nothing Nothing
 
--- | Unwrap Single values
-unwrapSingle :: Single a -> a
-unwrapSingle (Single v) = v
-
--- | Generic classifier query helpers
 getAllClassifiers :: ConnectionPool -> Text -> IO [ClassifierItem]
 getAllClassifiers pool table = do
   let sql = "SELECT id, code, name, description, parent_id FROM " <> T.unpack table <> " ORDER BY id"
   rows <- runDb pool $ rawSql sql [] :: IO [Single PersistValue]
-  return $ map (parseClassifier . (:[]) . unwrapSingle) rows
+  return $ map parseClassifier rows
 
 getClassifierById :: ConnectionPool -> Text -> Int64 -> IO (Maybe ClassifierItem)
 getClassifierById pool table id' = do
   let sql = "SELECT id, code, name, description, parent_id FROM " <> T.unpack table <> " WHERE id = ?"
   rows <- runDb pool $ rawSql sql [PersistInt64 id'] :: IO [Single PersistValue]
   case rows of
-    (row : _) -> return $ Just $ parseClassifier [unwrapSingle row]
+    (row : _) -> return $ Just $ parseClassifier row
     _ -> return Nothing
 
 getClassifierByCode :: ConnectionPool -> Text -> Text -> IO (Maybe ClassifierItem)
@@ -116,55 +76,40 @@ getClassifierByCode pool table code = do
   let sql = "SELECT id, code, name, description, parent_id FROM " <> T.unpack table <> " WHERE code = ?"
   rows <- runDb pool $ rawSql sql [PersistText code] :: IO [Single PersistValue]
   case rows of
-    (row : _) -> return $ Just $ parseClassifier [unwrapSingle row]
+    (row : _) -> return $ Just $ parseClassifier row
     _ -> return Nothing
 
 getClassifierByParent :: ConnectionPool -> Text -> Int64 -> IO [ClassifierItem]
 getClassifierByParent pool table parent = do
   let sql = "SELECT id, code, name, description, parent_id FROM " <> T.unpack table <> " WHERE parent_id = ?"
   rows <- runDb pool $ rawSql sql [PersistInt64 parent] :: IO [Single PersistValue]
-  return $ map (\row -> parseClassifier [unwrapSingle row]) rows
+  return $ map parseClassifier rows
 
--- OKSM
 getOksmAll = getAllClassifiers "oksm"
 getOksmById = getClassifierById "oksm"
 getOksmByCode = getClassifierByCode "oksm"
-
--- OKV
 getOkvAll = getAllClassifiers "okv"
 getOkvById = getClassifierById "okv"
 getOkvByCode = getClassifierByCode "okv"
-
--- OKEI
 getOkeiAll = getAllClassifiers "okei"
 getOkeiById = getClassifierById "okei"
 getOkeiByCode = getClassifierByCode "okei"
-
--- OKPD2
 getOkpd2All = getAllClassifiers "okpd2"
 getOkpd2ById = getClassifierById "okpd2"
 getOkpd2ByCode = getClassifierByCode "okpd2"
 getOkpd2ByParent = getClassifierByParent "okpd2"
-
--- OKVED2
 getOkved2All = getAllClassifiers "okved2"
 getOkved2ById = getClassifierById "okved2"
 getOkved2ByCode = getClassifierByCode "okved2"
 getOkved2ByParent = getClassifierByParent "okved2"
-
--- TNVED
 getTnvedAll = getAllClassifiers "tnved"
 getTnvedById = getClassifierById "tnved"
 getTnvedByCode = getClassifierByCode "tnved"
 getTnvedByParent = getClassifierByParent "tnved"
-
--- OKATO
 getOkatoAll = getAllClassifiers "okato"
 getOkatoById = getClassifierById "okato"
 getOkatoByCode = getClassifierByCode "okato"
 getOkatoByParent = getClassifierByParent "okato"
-
--- OKTMO
 getOktmoAll = getAllClassifiers "oktmo"
 getOktmoById = getClassifierById "oktmo"
 getOktmoByCode = getClassifierByCode "oktmo"
