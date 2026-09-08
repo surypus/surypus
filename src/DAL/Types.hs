f85c292598b3d3f9cec8ead8d9e86d54637c8009
Domini Montessori <dominicusin@duck.com>
2026-09-08 16:57:57 +0300

    fix: add Int64 import to DAL.Types

--- src/DAL/Types.hs
+++ src/DAL/Types.hs
@@ -12,6 +12,7 @@
 module DAL.Types where
 
 import Data.Aeson (FromJSON, ToJSON)
+import Data.Int (Int64)
 import qualified Data.Text as T
 import Data.Time (Day, UTCTime)
 import GHC.Generics (Generic)
